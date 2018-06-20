c107ba4fca4830a6fc9d44b9c0a504735c3c3fb7
crypto-jones <integral.being@gmail.com>
2018-06-20 12:41:35 -0700

    modify draw.py add graph.py

--- src/draw.py
+++ src/draw.py
@@ -5,27 +5,46 @@ from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
 
-N = 8
+from graph import *
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+print(graph_data.vertexes)
+
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1, 1.1), y_range=(-1.1, 1.1),
+color_list = []
+for vertex in graph_data.vertexes:
+    color_list.append(vertex.color)
+
+debug_palette = Spectral8
+debug_palette.append('#ff0000')
+debug_palette.append('#0000ff')
+
+plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
+graph.node_renderer.data_source.add(debug_palette, 'color')
 graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
 
+# this is drawing the edges from start to end
+#
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    start=[0]*N,  # this is a list that has to do with starting points
+    end=node_indices)  # this is a list tha ths to do with ending points
+
 
 # start of layout code
 # Looks like this is setting the position of the vertices
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+circ = [i*2*math.pi/N for i in node_indices]
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
+
+print(y)
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
