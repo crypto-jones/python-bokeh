24efcb9e2315a15b19b8faea5d267a592dfd12a7
crypto-jones <integral.being@gmail.com>
2018-06-20 15:30:52 -0700

    edit start and end points

--- src/draw.py
+++ src/draw.py
@@ -2,7 +2,7 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, LabelSet
 from bokeh.palettes import Spectral8
 
 from graph import *
@@ -18,36 +18,33 @@ color_list = []
 for vertex in graph_data.vertexes:
     color_list.append(vertex.color)
 
-debug_palette = Spectral8
-debug_palette.append('#ff0000')
-debug_palette.append('#0000ff')
-
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+plot = figure(title='Graph Layout Demonstration', x_axis_label='Time', y_axis_label='Price', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(debug_palette, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.data_source.add(color_list, 'color')
+graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
-# this is drawing the edges from start to end
-#
+#########  1. this is drawing the edges from start to end ########
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,  # this is a list that has to do with starting points
-    end=node_indices)  # this is a list tha ths to do with ending points
+    # this is a list of some kind that has to do with starting points
+    start=node_indices,
+    end=[1, 2, 3, 4]
+)  # this is a list of some kind that has to do with ending points
 
 
+print(graph.edge_renderer.data_source.data)
 # start of layout code
-# Looks like this is setting the position of the vertices
-circ = [i*2*math.pi/N for i in node_indices]
+# change from 8 to N to make it dynamic
+
 x = [v.pos['x'] for v in graph_data.vertexes]
 y = [v.pos['y'] for v in graph_data.vertexes]
 
-print(y)
-
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
 
 plot.renderers.append(graph)
 
